05159accf4f03cf702d90026e279db5648088f24
Ahmed Samir <ahmedsamir551@gmail.com>
2022-01-31 16:56:13 +0200

    Add files via upload

---
+++ Udemy/2-Complete-React-Developer-Course/Section12/src/playground/destructuring.js
@@ -0,0 +1,38 @@
+const person = {
+    name: "ahmed",
+    age: 29,
+    location: {
+      city: "cairo",
+      temp: 25,
+    },
+  };
+  
+  const { name: firstName = "anonymous", age } = person;
+  console.log(`${firstName} is ${age}`);
+  
+  const { city, temp: temperature } = person.location;
+  if (city && temperature) {
+    console.log(`its ${temperature} in ${city}`);
+  }
+  
+  const book = {
+    title: "ego is the enemy",
+    author: "ryan holiday",
+    publisher: {
+      name: "penguin",
+    },
+  };
+  
+  const { name: publisherName = "self published" } = book.publisher;
+  
+  console.log(publisherName);
+  
+  const address = ["20 s juniper street", "alex", "cairo", "29"];
+  const [, city, state = "new york"] = address;
+  console.log(`you are in ${city} ${state}`);
+  
+  const item = ["coffee", "$3.00", "$3.50", "$3.75"];
+  const [itemName, , mediumPrice] = item;
+  
+  console.log(`a medium ${itemName} costs ${mediumPrice}`);
+  
